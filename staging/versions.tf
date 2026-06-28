ee09e86f86eb5e568aea9e1542a02355a47c7a33
AhsanRahat12 <s.rahatahsan@gmail.com>
2026-06-28 13:56:34 -0500

    Added the verions terraform file

---
+++ staging/versions.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_version = ">= 1.0"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.0"
+    }
+    local = {
+      source  = "hashicorp/local"
+      version = "~> 2.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+  subscription_id = "7b46fac3-e7d4-4c64-aa70-5a5200983051"
+}
